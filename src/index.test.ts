74cc233d70bbf72dd3e58f33928046850aef7a1f
Jason Divock <jdivock@stripe.com>
2020-04-28 11:51:24 -0700

    more tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,11 +2,11 @@
 
 const dispatchScriptEvent = (eventType: string): void => {
   const injectedScript = document.querySelector(
-    'script[src="https://js.stripe.com/v3"]'
+    'script[src="https://js.stripe.com/terminal/v1"]'
   );
 
   if (!injectedScript) {
-    throw new Error('could not find Stripe.js script element');
+    throw new Error('could not find StripeTerminal script element');
   }
 
   injectedScript.dispatchEvent(new Event(eventType));
@@ -15,7 +15,7 @@ const dispatchScriptEvent = (eventType: string): void => {
 describe('Stripe module loader', () => {
   afterEach(() => {
     const script = document.querySelector(
-      'script[src="https://js.stripe.com/v3"], script[src="https://js.stripe.com/v3/"]'
+      'script[src="https://js.stripe.com/terminal/v1"], script[src="https://js.stripe.com/terminal/v1/"]'
     );
     if (script && script.parentElement) {
       script.parentElement.removeChild(script);
@@ -24,16 +24,18 @@ describe('Stripe module loader', () => {
     jest.resetModules();
   });
 
-  it('injects the Stripe script as a side effect after a tick', () => {
+  it('injects the Terminal script as a side effect after a tick', () => {
     require('./index');
 
     expect(
-      document.querySelector('script[src="https://js.stripe.com/v3"]')
+      document.querySelector('script[src="https://js.stripe.com/terminal/v1"]')
     ).toBe(null);
 
     return Promise.resolve().then(() => {
       expect(
-        document.querySelector('script[src="https://js.stripe.com/v3"]')
+        document.querySelector(
+          'script[src="https://js.stripe.com/terminal/v1"]'
+        )
       ).not.toBe(null);
     });
   });
@@ -45,7 +47,9 @@ describe('Stripe module loader', () => {
 
     return new Promise((resolve) => setTimeout(resolve)).then(() => {
       expect(
-        document.querySelector('script[src="https://js.stripe.com/v3"]')
+        document.querySelector(
+          'script[src="https://js.stripe.com/terminal/v1"]'
+        )
       ).toBe(null);
     });
   });
@@ -55,13 +59,13 @@ describe('Stripe module loader', () => {
       require('./index');
 
       const script = document.createElement('script');
-      script.src = 'https://js.stripe.com/v3';
+      script.src = 'https://js.stripe.com/terminal/v1';
       document.body.appendChild(script);
 
       return Promise.resolve().then(() => {
         expect(
           document.querySelectorAll(
-            'script[src="https://js.stripe.com/v3"], script[src="https://js.stripe.com/v3/"]'
+            'script[src="https://js.stripe.com/terminal/v1"], script[src="https://js.stripe.com/terminal/v1/"]'
           )
         ).toHaveLength(1);
       });
@@ -71,64 +75,67 @@ describe('Stripe module loader', () => {
       require('./index');
 
       const script = document.createElement('script');
-      script.src = 'https://js.stripe.com/v3/';
+      script.src = 'https://js.stripe.com/terminal/v1/';
       document.body.appendChild(script);
 
       return Promise.resolve().then(() => {
         expect(
           document.querySelectorAll(
-            'script[src="https://js.stripe.com/v3"], script[src="https://js.stripe.com/v3/"]'
+            'script[src="https://js.stripe.com/terminal/v1"], script[src="https://js.stripe.com/terminal/v1/"]'
           )
         ).toHaveLength(1);
       });
     });
   });
 
-  describe.each(['./index', './pure'])('loadStripe (%s.ts)', (requirePath) => {
-    beforeEach(() => {
-      jest.restoreAllMocks();
-      jest.spyOn(console, 'warn').mockReturnValue();
-    });
+  describe.each(['./index', './pure'])(
+    'loadStripeTerminal (%s.ts)',
+    (requirePath) => {
+      beforeEach(() => {
+        jest.restoreAllMocks();
+        jest.spyOn(console, 'warn').mockReturnValue();
+      });
 
-    it('resolves loadStripe with Stripe object', async () => {
-      const {loadStripe} = require(requirePath);
-      const stripePromise = loadStripe('pk_test_foo');
+      it('resolves loadStripeTerminal with Terminal object', async () => {
+        const {loadStripeTerminal} = require(requirePath);
+        const stripePromise = loadStripeTerminal();
 
-      await new Promise((resolve) => setTimeout(resolve));
-      window.StripeTerminal = jest.fn((key) => ({key})) as any;
-      dispatchScriptEvent('load');
+        await new Promise((resolve) => setTimeout(resolve));
+        window.StripeTerminal = jest.fn() as any;
+        dispatchScriptEvent('load');
 
-      return expect(stripePromise).resolves.toEqual({key: 'pk_test_foo'});
-    });
+        return expect(stripePromise).resolves.toBeDefined();
+      });
 
-    it('rejects when the script fails', async () => {
-      const {loadStripe} = require(requirePath);
-      const stripePromise = loadStripe('pk_test_foo');
+      it('rejects when the script fails', async () => {
+        const {loadStripeTerminal} = require(requirePath);
+        const stripePromise = loadStripeTerminal();
 
-      await Promise.resolve();
-      dispatchScriptEvent('error');
+        await Promise.resolve();
+        dispatchScriptEvent('error');
 
-      await expect(stripePromise).rejects.toEqual(
-        new Error('Failed to load Stripe.js')
-      );
+        await expect(stripePromise).rejects.toEqual(
+          new Error('Failed to load StripeTerminal')
+        );
 
-      expect(console.warn).not.toHaveBeenCalled();
-    });
+        expect(console.warn).not.toHaveBeenCalled();
+      });
 
-    it('rejects when Stripe is not added to the window for some reason', async () => {
-      const {loadStripe} = require(requirePath);
-      const stripePromise = loadStripe('pk_test_foo');
+      it('rejects when Terminal is not added to the window for some reason', async () => {
+        const {loadStripeTerminal} = require(requirePath);
+        const stripePromise = loadStripeTerminal();
 
-      await Promise.resolve();
-      dispatchScriptEvent('load');
+        await Promise.resolve();
+        dispatchScriptEvent('load');
 
-      return expect(stripePromise).rejects.toEqual(
-        new Error('Stripe.js not available')
-      );
-    });
-  });
+        return expect(stripePromise).rejects.toEqual(
+          new Error('StripeTerminal not available')
+        );
+      });
+    }
+  );
 
-  describe('loadStripe (index.ts)', () => {
+  describe('loadStripeTerminal (index.ts)', () => {
     it('does not cause unhandled rejects when the script fails', async () => {
       require('./index');
 
@@ -139,7 +146,7 @@ describe('Stripe module loader', () => {
       await new Promise((resolve) => setImmediate(resolve));
 
       expect(console.warn).toHaveBeenCalledWith(
-        new Error('Failed to load Stripe.js')
+        new Error('Failed to load StripeTerminal')
       );
     });
   });
