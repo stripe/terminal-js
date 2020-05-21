4e9a64a5438c9537e9f03a980b0a3bb4d8ffce50
Jason Divock <jdivock@stripe.com>
2020-05-21 15:55:00 -0700

    exporting all types from pure as well

--- pure.d.ts
+++ pure.d.ts
@@ -1,3 +1,11 @@
 import {loadStripeTerminal as IloadStripeTerminal} from './src/pure';
+import {TerminalProps} from './types/terminal-js/rabbit/terminal-props';
+import {Terminal} from './types/terminal-js/index';
+
+export * from './types/terminal-js/index';
+
+export interface StripeTerminal {
+  create(props: TerminalProps): Terminal;
+}
 
 export const loadStripeTerminal: typeof IloadStripeTerminal;
