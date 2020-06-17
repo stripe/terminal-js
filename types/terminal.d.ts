029a6ac1eb612b5a0d98c6ea01c60b8a6c832b62
Jason Divock <jdivock@stripe.com>
2020-06-17 14:08:51 -0700

    going back to enums with guidance in readme

--- types/terminal.d.ts
+++ types/terminal.d.ts
@@ -51,15 +51,19 @@ export interface TerminalOptions {
 export declare type TerminalProps = TerminalOptions & TerminalCallbacks;
 
 export declare type PaymentIntentClientSecret = string;
-export declare type PaymentStatus =
-  | 'not_ready'
-  | 'ready'
-  | 'waiting_for_input'
-  | 'processing';
-export declare type ConnectionStatus =
-  | 'connecting'
-  | 'connected'
-  | 'not_connected';
+
+export enum PaymentStatus {
+  NOT_READY = 'not_ready',
+  READY = 'ready',
+  WAITING_FOR_INPUT = 'waiting_for_input',
+  PROCESSING = 'processing',
+}
+
+export enum ConnectionStatus {
+  CONNECTING = 'connecting',
+  CONNECTED = 'connected',
+  NOT_CONNECTED = 'not_connected',
+}
 
 // eslint-disable-next-line
 export interface ISdkManagedPaymentIntent extends IPaymentIntent {
